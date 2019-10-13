6489d57a1613f1be568df5ea75cd84089f400593
vishal-verma14 <vishal-verma14@users.noreply.github.com>
2019-10-14 00:57:54 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,12 +4,7 @@ import firestore from 'firebase/firestore';
 const settings = { timestampsInSnapshots: true };
 
 const config = {
-	   apiKey: "AIzaSyABxTdiGmtVb1nSR9aSAknLkfldhM_WCIg",
-    authDomain: "rentpura.firebaseapp.com",
-    databaseURL: "https://rentpura.firebaseio.com",
-    projectId: "rentpura",
-    storageBucket: "rentpura.appspot.com",
-    messagingSenderId: "213433009415"
+	  "Enter Your credentials"
 };
 firebase.initializeApp(config);
 
